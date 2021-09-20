03a70ba3df5d4764d25edd2927481cd706a960cc
Luan Dias <luoliveidias@gmail.com>
2021-09-20 13:05:27 -0300

    Validacao sintatica com Hibernate validator

--- src/main/java/com/luandias/cursomc/services/CategoriaService.java
+++ src/main/java/com/luandias/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.luandias.cursomc.domain.Categoria;
+import com.luandias.cursomc.dto.CategoriaDTO;
 import com.luandias.cursomc.repositories.CategoriaRepository;
 import com.luandias.cursomc.servicies.exceptions.DataIntegrityException;
 import com.luandias.cursomc.servicies.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
