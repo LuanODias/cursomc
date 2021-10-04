e45549a14493cd453cb929e5f7ef5e404dc1eb46
Luan Dias <luoliveidias@gmail.com>
2021-10-04 13:47:26 -0300

    Inserindo novo cliente com POST

--- src/main/java/com/luandias/cursomc/repositories/EnderecoRepository.java
+++ src/main/java/com/luandias/cursomc/repositories/EnderecoRepository.java
@@ -1,5 +1,7 @@
 package com.luandias.cursomc.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.luandias.cursomc.domain.Endereco;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+	void save(List<Endereco> enderecos);
 
 	
 }
