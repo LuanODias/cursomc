3e0020a0413043414d0034090f72d5746bc99216
Luan Dias <luoliveidias@gmail.com>
2021-10-22 16:05:50 -0300

    validacao customizada: email nao repetido na inserção Cliente

--- src/main/java/com/luandias/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/luandias/cursomc/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.luandias.cursomc.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import com.luandias.cursomc.domain.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
 	
+	Cliente findByEmail(String email);
 }
